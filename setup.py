d870945b4ef40b01b4af4376b2c41fb990f96dd5
raphamatoss <raphaelmsgoncalves@gmail.com>
2025-11-15 18:40:37 -0300

    fix: missing comma

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
         'numpy>=2.2.1,<2.3.3',
         'Pillow!=8.3.*,>=5.3.0',
         'requests>=2.0.0,<3.0.0',
-        'torch>=2.6.0'
+        'torch>=2.6.0',
         'torchvision>=0.17.0,<0.25.0',
         'tqdm>=4.64.1,<4.66.5',
     ],
